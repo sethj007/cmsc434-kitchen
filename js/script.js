26b6d1f11433f763c815a8a5e40d09dd901c6cab
Seth Johnston <sjohnston@seths-macbook.local>
2026-02-25 10:40:18 -0500

    done tab 5

--- js/script.js
+++ js/script.js
@@ -11,3 +11,10 @@ tabs.forEach(tab => {
     document.getElementById(tab.dataset.target).classList.add('active');
   });
 });
+
+document.getElementById('choices-btn').addEventListener('click', () => {
+    const camera1 = document.querySelector('input[name="camera1"]:checked').value;
+    const camera2 = document.getElementById('camera2').value;
+    document.getElementById('choices-result').textContent = 
+        `You chose ${camera1} for #1 and ${camera2} for #2!`
+})
